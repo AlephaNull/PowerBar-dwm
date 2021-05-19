33a673c40e94ce07c2cb9cd723cd950bbe0af7e0
HashTag-4512 <hashtag25april@gmail.com>
2021-05-19 14:01:27 +0500

    Added more funcs

--- PowerStatusColor.sh
+++ PowerStatusColor.sh
@@ -16,13 +16,34 @@ color5="\x07"
 
 #main functions
 datetime(){
-  date_time="$(date '+%d-%m-%Y  %H:%M')" #date and time in dd-mm-yyy HH:MM format
-  printf "%s%s" "$(echo -e $color2 $powerline_h)" "$(echo -e $color3 $date_time)"
+  date_time="$(date '+%d-%m-%Y   %H:%M')" #date and time in dd-mm-yyy HH:MM format
+  printf "%s%s" "$(echo -e $color2 $powerline_h)" "$(echo -e $color3 "" $date_time)"
+}
+
+sound(){
+  vol="$(amixer get Master | sed -n 's/^.*\[\([0-9]\+\)%.*$/\1/p'| uniq)"
+  printf "%s%s" "$(echo -e $color1 $powerline_h)" "$(echo -e $color4 "" $vol'% ')"
+}
+
+battery(){
+  batc="$(cat  /sys/class/power_supply/BAT0/capacity)"   # contains a value from 0 to 100
+  bats="$(cat /sys/class/power_supply/BAT0/status)"     # either "Charging" or "Discharging" 
+  printf "%s%s %s" "$(echo -e $color2 $powerline_h)" "$(echo -e $color3 "" $batc'%')" "$(echo -e $color3 $bats)"
+}
+
+memory(){
+  ram="$(free -t | awk 'FNR == 2 {print ""$3/$2*100}')"
+  printf "%s%s" "$(echo -e $color1 $powerline_h)" "$(echo -e $color4 "" $ram'% ')"
+}
+
+process(){
+  cpu="$(ps -eo pcpu | awk 'BEGIN {sum=0.0f} {sum+=$1} END {print sum}')"
+  printf "%s%s" "$(echo -e $color5 $powerline_h)" "$(echo -e $color3 "" $cpu'%')"
 }
 
 #update every 30 seconds
 while true; do
-	xsetroot -name "$(datetime)"
+  xsetroot -name "$(process) $(memory) $(battery) $(sound) $(datetime)"
 	sleep 30
 done
 
